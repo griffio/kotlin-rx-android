ef0360c97e2fb7241a2049f83bfb9bb8b258396c
griffio <griffio@users.noreply.github.com>
2015-03-29 21:57:57 +0100

    kotlin butterknife

--- app/src/main/kotlin/griffio/activities/MainActivity.kt
+++ app/src/main/kotlin/griffio/activities/MainActivity.kt
@@ -4,33 +4,24 @@ import android.os.Bundle
 import android.support.v7.app.ActionBarActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.EditText
+import android.widget.TextView
 import griffio.application.R
+import butterknife.bindView
 
 open class MainActivity : ActionBarActivity() {
+
+    val title : TextView by bindView(R.id.title)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        title.setText("Efficiently unleash cross-media information without cross-media value. Quickly maximize timely deliverables for real-time schemas. Dramatically maintain clicks-and-mortar solutions without functional solutions.")
     }
 
-
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater()?.inflate(R.menu.menu_main, menu)
         return true
     }
 
-    override fun onOptionsItem                                                                                                                                                                                         Selected(item: MenuItem?): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        val id = item?.getItemId()
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true
-        }
-
-        return super.onOptionsItemSelected(item)
-    }
 }
---
+++ app/src/main/kotlin/griffio/application/MainApplication.kt
@@ -0,0 +1,9 @@
+package griffio.application
+
+import android.app.Application
+
+open class MainApplication: Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
